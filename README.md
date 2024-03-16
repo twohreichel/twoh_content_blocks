9845a4deda9673772a4ff60f71955ec2a8998146
Andreas Reichel <a.reichel@schubwerk.de>
2024-03-16 09:07:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## TWOH Content Blocks
+Extension containing a list of basic TYPO3 Content Elements based on Content Blocks.
+
+### Minimum requirements
+* **PHP** 8
+* **composer** ^2
+* **TYPO3** 12
+
+### Setup
+* install Extension via Composer or FTP
+* include Extension in TypoScript **ROOT Template**
---
+++ ext_emconf.php
@@ -0,0 +1,29 @@
+<?php
+
+$EM_CONF['twoh_content_blocks'] = array(
+    'title' => 'TWOH TYPO3 Content Blocks',
+    'description' => 'Extension containing a list of basic TYPO3 Content Elements based on Content Blocks.',
+    'category' => 'plugin',
+    'author' => 'Andreas Reichel',
+    'author_email' => 'a.reichel91@outlook.com',
+    'author_company' => 'TWOH digital',
+    'shy' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'version' => '1.0.0',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '12.4.99',
+            'php' => '8.0-8.3',
+        ),
+        'conflicts' => array(),
+        'suggests' => array(),
+    ),
+);
